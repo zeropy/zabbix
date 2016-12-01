c7e487df6ec7e06cc297fe445a2ec6a8d1f2f3e9
legion <1931330336@qq.com>
2016-12-01 10:54:15 +0800

    rename dirtory script to scripts

---
+++ scripts/chk_mysql_slave.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+# Revision : 1.0
+# Date : 2016/10/27
+# Author : Legion
+# Email : 1931330336@qq.com
+# Description:
+# License: GPL
+
+# 用户名
+MYSQL_USER='zabbix'
+
+# 密码
+MYSQL_PASS='123456'
+
+# IP地址
+MYSQL_HOST='127.0.0.1'
+
+# 端口
+MYSQL_PORT='3306'
+
+# 数据库连接
+MYSQL_CONN="/usr/bin/mysql -u${MYSQL_USER} -p${MYSQL_PASS} -h${MYSQL_HOST} -P${MYSQL_PORT}"
+
+# 参数是否正确
+if [ $# -ne "1" ];then
+    echo "arg error!" 
+fi
+
+case $1 in
+    IO)
+        $MYSQL_CONN -e 'show slave status\G' | grep 'Slave_IO_Running' | grep -c 'Yes'
+        ;;
+    SQL)
+        $MYSQL_CONN -e 'show slave status\G' | grep 'Slave_SQL_Running' | grep -c 'Yes'
+        ;;
+    LAG)
+        $MYSQL_CONN -e 'show slave status\G' | grep 'Seconds_Behind_Master' | awk -F: '{print $2}'
+        ;;
+    *)
+        echo "Usage:$0 (IO|SQL)"
+        ;;
+esac
+
